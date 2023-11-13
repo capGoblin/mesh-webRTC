eab7f5b9150b6b4023c101c6c866e6012a9a0c4d
capGoblin <dharshan2457@gmail.com>
2023-11-13 14:20:17 +0530

    update clerk auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,234 +1,25 @@
-import React, { useState, useRef, useEffect, useCallback } from "react";
-import io from "socket.io-client";
-import Video from "./Components/Video";
-import { WebRTCUser } from "./types";
-import Draw from "./Components/Draw";
-import TextEditor from "./Components/TextEditor";
-// import Draw from "./Components/Draw";
+import React from "react";
+import HomePage from "./Components/HomePage";
 
-const pc_config = {
-  iceServers: [
-    // {
-    //   urls: 'stun:[STUN_IP]:[PORT]',
-    //   'credentials': '[YOR CREDENTIALS]',
-    //   'username': '[USERNAME]'
-    // },
-    {
-      urls: "stun:stun.l.google.com:19302",
-    },
-  ],
-};
-const SOCKET_SERVER_URL = "http://localhost:8080";
-const socket = io(SOCKET_SERVER_URL);
-
-const App = () => {
-  // const socketRef = useRef<Socket>();
-  const pcsRef = useRef<{ [socketId: string]: RTCPeerConnection }>({});
-  const localVideoRef = useRef<HTMLVideoElement>(null);
-  const localStreamRef = useRef<MediaStream>();
-  const [users, setUsers] = useState<WebRTCUser[]>([]);
-
-  const getLocalStream = useCallback(async () => {
-    try {
-      const localStream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: {
-          width: 240,
-          height: 240,
-        },
-      });
-      localStreamRef.current = localStream;
-      if (localVideoRef.current) localVideoRef.current.srcObject = localStream;
-      if (!socket) return;
-      socket.emit("join_room", {
-        room: "1234",
-        email: "sample@naver.com",
-      });
-    } catch (e) {
-      console.log(`getUserMedia error: ${e}`);
-    }
-  }, []);
-
-  const createPeerConnection = useCallback(
-    (socketID: string, email: string) => {
-      try {
-        const pc = new RTCPeerConnection(pc_config);
-
-        pc.onicecandidate = (e) => {
-          if (!(socket && e.candidate)) return;
-          console.log("onicecandidate");
-          socket.emit("candidate", {
-            candidate: e.candidate,
-            candidateSendID: socket.id,
-            candidateReceiveID: socketID,
-          });
-        };
-
-        pc.oniceconnectionstatechange = (e) => {
-          console.log(e);
-        };
-
-        pc.ontrack = (e) => {
-          console.log("ontrack success");
-          setUsers((oldUsers) =>
-            oldUsers
-              .filter((user) => user.id !== socketID)
-              .concat({
-                id: socketID,
-                email,
-                stream: e.streams[0],
-              })
-          );
-        };
-
-        if (localStreamRef.current) {
-          console.log("localstream add");
-          localStreamRef.current.getTracks().forEach((track) => {
-            if (!localStreamRef.current) return;
-            pc.addTrack(track, localStreamRef.current);
-          });
-        } else {
-          console.log("no local stream");
-        }
-
-        return pc;
-      } catch (e) {
-        console.error(e);
-        return undefined;
-      }
-    },
-    []
-  );
-
-  useEffect(() => {
-    getLocalStream();
-
-    socket.on("all_users", (allUsers: Array<{ id: string; email: string }>) => {
-      allUsers.forEach(async (user) => {
-        if (!localStreamRef.current) return;
-        const pc = createPeerConnection(user.id, user.email);
-        if (!(pc && socket)) return;
-        pcsRef.current = { ...pcsRef.current, [user.id]: pc };
-        try {
-          const localSdp = await pc.createOffer({
-            offerToReceiveAudio: true,
-            offerToReceiveVideo: true,
-          });
-          console.log("create offer success");
-          await pc.setLocalDescription(new RTCSessionDescription(localSdp));
-          socket.emit("offer", {
-            sdp: localSdp,
-            offerSendID: socket.id,
-            offerSendEmail: "offerSendSample@sample.com",
-            offerReceiveID: user.id,
-          });
-        } catch (e) {
-          console.error(e);
-        }
-      });
-    });
-
-    socket.on(
-      "getOffer",
-      async (data: {
-        sdp: RTCSessionDescription;
-        offerSendID: string;
-        offerSendEmail: string;
-      }) => {
-        const { sdp, offerSendID, offerSendEmail } = data;
-        console.log("get offer");
-        if (!localStreamRef.current) return;
-        const pc = createPeerConnection(offerSendID, offerSendEmail);
-        if (!(pc && socket)) return;
-        pcsRef.current = { ...pcsRef.current, [offerSendID]: pc };
-        try {
-          await pc.setRemoteDescription(new RTCSessionDescription(sdp));
-          console.log("answer set remote description success");
-          const localSdp = await pc.createAnswer({
-            offerToReceiveVideo: true,
-            offerToReceiveAudio: true,
-          });
-          await pc.setLocalDescription(new RTCSessionDescription(localSdp));
-          socket.emit("answer", {
-            sdp: localSdp,
-            answerSendID: socket.id,
-            answerReceiveID: offerSendID,
-          });
-        } catch (e) {
-          console.error(e);
-        }
-      }
-    );
-
-    socket.on(
-      "getAnswer",
-      (data: { sdp: RTCSessionDescription; answerSendID: string }) => {
-        const { sdp, answerSendID } = data;
-        console.log("get answer");
-        const pc: RTCPeerConnection = pcsRef.current[answerSendID];
-        if (!pc) return;
-        pc.setRemoteDescription(new RTCSessionDescription(sdp));
-      }
-    );
-
-    socket.on(
-      "getCandidate",
-      async (data: {
-        candidate: RTCIceCandidateInit;
-        candidateSendID: string;
-      }) => {
-        console.log("get candidate");
-        const pc: RTCPeerConnection = pcsRef.current[data.candidateSendID];
-        if (!pc) return;
-        await pc.addIceCandidate(new RTCIceCandidate(data.candidate));
-        console.log("candidate add success");
-      }
-    );
-
-    socket.on("user_exit", (data: { id: string }) => {
-      if (!pcsRef.current[data.id]) return;
-      pcsRef.current[data.id].close();
-      delete pcsRef.current[data.id];
-      setUsers((oldUsers) => oldUsers.filter((user) => user.id !== data.id));
-    });
-
-    return () => {
-      if (socket) {
-        socket.disconnect();
-      }
-      users.forEach((user) => {
-        if (!pcsRef.current[user.id]) return;
-        pcsRef.current[user.id].close();
-        delete pcsRef.current[user.id];
-      });
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [createPeerConnection, getLocalStream]);
+function App() {
+  // const navigate = useNavigate();
+  //
+  // function handleSignInClickEvent() {
+  //   navigate("/sign-in/");
+  // }
+  //
+  // function handleSignUpClickEvent() {
+  //   navigate("/sign-up/");
+  // }
 
   return (
     <>
-      <div>
-        <video
-          style={{
-            width: 240,
-            height: 240,
-            margin: 5,
-            backgroundColor: "black",
-          }}
-          muted
-          ref={localVideoRef}
-          autoPlay
-        />
-        {users.map((user, index) => (
-          <Video key={index} email={user.email} stream={user.stream} />
-        ))}
-      </div>
-      <div>
-        <TextEditor />
-        {/* <Draw /> */}
-      </div>
+      {/*<button onClick={handleSignInClickEvent}>Sign In</button>*/}
+      {/*<button onClick={handleSignUpClickEvent}>Sign Up</button>*/}
+
+      <HomePage />
     </>
   );
-};
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import {Route, Routes, useNavigate} from "react-router-dom";
+// import {ClerkProvider, RedirectToSignIn, SignedIn, SignedOut, SignIn, SignUp,} from "@clerk/clerk-react";
+import App from "./App";
+// import HomePage from "./components/HomePage.tsx";
 
-ReactDOM.render(
+// if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
+//   throw new Error("Missing Publishable Key");
+// }
+// const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+// function ClerkProviderWithRoutes() {
+//   const navigate = useNavigate();
+
+//   return (
+//     <ClerkProvider publishableKey={clerkPubKey} navigate={(to) => navigate(to)}>
+//       <Routes>
+//         <Route path="/" element={<App/>}/>
+//         <Route
+//           path="/sign-in/*"
+//           element={<SignIn redirectUrl="/homepage" path="/sign-in"/>}
+//         />
+//         <Route
+//           path="/sign-up/*"
+//           element={
+//             <SignUp redirectUrl="/homepage" routing="path" path="/sign-up"/>
+//           }
+//         />
+//         <Route
+//           path="/homepage"
+//           element={
+//             <>
+//               <SignedIn>
+//                 <HomePage/>
+//               </SignedIn>
+//               <SignedOut>
+//                 <RedirectToSignIn/>
+//               </SignedOut>
+//             </>
+//           }
+//         />
+//         <Route path="/homepage" element={<HomePage/>}/>
+//       </Routes>
+//     </ClerkProvider>
+//   );
+// }
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <App/>
+    {/*<BrowserRouter>*/}
+    {/*  <ClerkProviderWithRoutes />*/}
+    {/*</BrowserRouter>*/}
   </React.StrictMode>,
-  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
